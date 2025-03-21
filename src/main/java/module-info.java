8153865a01da48818b10ffb3340aa4d0f488c4d7
Harumb1 <miroslav.mihaylov.pcj@gmail.com>
2025-03-21 23:16:57 +0200

    Changed background, added icon and a label which doesn't work!

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module org.example.taskmanagerlist {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.desktop;
 
     opens org.example.taskmanagerlist to javafx.fxml;
     exports org.example.taskmanagerlist;
--- src/main/java/org/example/taskmanagerlist/HelloApplication.java
+++ src/main/java/org/example/taskmanagerlist/HelloApplication.java
@@ -1,23 +1,40 @@
 package org.example.taskmanagerlist;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Task Manager");
+        GridPane root = new GridPane();
+        Scene scene = new Scene(root, Color.BLACK);
+        Image icon = new Image("C:\\Users\\alexs\\IdeaProjects\\TaskManagerList\\src\\letter_d_PNG13-2884847867.png");
+        Label l = new Label("  _______    _____              _     _      _ \n" +
+                " |__   __|  |  __ \\            | |   | |    | |\n" +
+                "    | | ___ | |  | | ___   __ _| |__ | | ___| |\n" +
+                "    | |/ _ \\| |  | |/ _ \\ / _` | '_ \\| |/ _ \\ |\n" +
+                "    | | (_) | |__| | (_) | (_| | |_) | |  __/_|\n" +
+                "    |_|\\___/|_____/ \\___/ \\__,_|_.__/|_|\\___(_)");
+
+        stage.getIcons().add(icon);
+        stage.setTitle("ToDoable!");
         stage.setScene(scene);
         stage.show();
+        stage.setHeight(420);
+        stage.setWidth(420);
+        stage.setResizable(true);
+
+
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
